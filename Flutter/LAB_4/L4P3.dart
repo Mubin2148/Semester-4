35829813aad39f27c14b2d768010b461d3c32110
Mubin2148 <95473779+Mubin2148@users.noreply.github.com>
2022-12-22 13:36:36 +0530

    Add files via upload

--- Flutter/LAB_4/L4P3.dart
+++ Flutter/LAB_4/L4P3.dart
@@ -1,1 +1,18 @@
+//Write a dart code to generate a Fibonacci series of N given numbers using this method.
 
+import 'dart:io';
+
+void main(List<String> args) {
+  print("enter the number of elements : ");
+  int? n = int.parse(stdin.readLineSync()!);
+  int n1 = 0, n2 = 1, n3;
+  print("$n1,");
+  print('$n2,');
+
+  for (int i = 2; i < n; i++) {
+    n3 = n1 + n2;
+    print('$n3,');
+    n1 = n2;
+    n2 = n3;
+  }
+}
