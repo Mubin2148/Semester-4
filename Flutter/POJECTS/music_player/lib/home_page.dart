e570a07ae3036cdca188db97e5edcbb2eb5a6a33
Mubin2148 <mubinseta1@gmail.com>
2023-02-09 17:52:46 +0530

    .

--- Flutter/POJECTS/music_player/lib/home_page.dart
+++ Flutter/POJECTS/music_player/lib/home_page.dart
@@ -6,119 +6,148 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(17, 20, 39, 1),
-      body: Column(
-        children: [
-          Container(
-              alignment: Alignment.topLeft,
-              child: Text(
-                "Find Your Best Music",
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.white,
-                  fontFamily: "Kanit",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromRGBO(17, 20, 39, 1),
+        body: Column(
+          children: [
+            Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Find Your Best Music",
+                  style: TextStyle(
+                    fontSize: 26,
+                    color: Colors.white,
+                    fontFamily: "Kanit",
+                  ),
+                ),
+                margin: EdgeInsets.fromLTRB(13, 23, 0, 15)),
+            Container(
+              margin: EdgeInsets.fromLTRB(13, 0, 13, 0),
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey,
+                  prefixIcon: Icon(Icons.search, color: Colors.white),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  hintText: ' Search',
                 ),
               ),
-              margin: EdgeInsets.fromLTRB(13, 23, 0, 15)),
-          Container(
-            margin: EdgeInsets.fromLTRB(13, 0, 13, 0),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.grey,
-                prefixIcon: Icon(Icons.search, color: Colors.white),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15, 15, 0, 0),
+                    child: Text(
+                      "New Albums",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontFamily: "Kanit"),
+                    ),
                   ),
-                ),
-                hintText: ' Search',
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 15, 15, 0),
+                    child: Text(
+                      "View all",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontFamily: "Kanit"),
+                    ),
+                  )
+                ],
               ),
             ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  padding: EdgeInsets.fromLTRB(15, 15, 0, 0),
-                  child: Text(
-                    "New Albums",
-                    style: TextStyle(
-                        color: Colors.white, fontSize: 20, fontFamily: "Kanit"),
+            Expanded(
+              child: ListView(
+                children: [
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        albumContainer("assets/images/peli.jpg", "Blessed", 7),
+                        albumContainer("assets/images/second.jpg", "Rhino", 7),
+                        albumContainer("assets/images/third.jpg", "Good", 7),
+                        albumContainer("assets/images/fourth.jpg", "Excell", 7),
+                        albumContainer("assets/images/fifth.jpg", " ", 7),
+                        albumContainer("assets/images/sixth.jpg", " ", 7),
+                        albumContainer("assets/images/seventh.jpg", " ", 7),
+                        albumContainer("assets/images/eightth.jpg", " ", 7),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(0, 15, 15, 0),
-                  child: Text(
-                    "View all",
-                    style: TextStyle(
-                        color: Colors.grey, fontSize: 14, fontFamily: "Kanit"),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "Recently Music",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontFamily: "Kanit"),
+                    ),
                   ),
-                )
-              ],
+                  recentMusic("assets/images/fourth.jpg", "Havana",
+                      "Camilla Cabello", "01"),
+                  recentMusic("assets/images/eightth.jpg", "Techno Music",
+                      "Essentiel Mix", "02"),
+                  recentMusic("assets/images/sixth.jpg", "Stargrows",
+                      "ittile Iselands", "03"),
+                  recentMusic(
+                      "assets/images/peli.jpg", "Orion", "Volta Music", "04"),
+                  recentMusic("assets/images/third.jpg",
+                      "Everything is Blessed", "Emilla Dine", "05"),
+                  recentMusic("assets/images/fifth.jpg", "Tukajo",
+                      "Redani Diana", "06"),
+                  recentMusic(
+                      "assets/images/second.jpg", "Cristiano", "Ronaldo", "07"),
+                  recentMusic(
+                      "assets/images/seventh.jpg", "Seranvika", "Grapes", "08"),
+                  recentMusic(
+                      "assets/images/fourth.jpg", "Dragons", "Fullilo", "09"),
+                  recentMusic(
+                      "assets/images/sixth.jpg", "Hersiye", "lions", "10"),
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView(
-              children: [
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      albumContainer("assets/images/peli.jpg", "text", 7),
-                      albumContainer("assets/images/second.jpg", "text", 7),
-                      albumContainer("assets/images/third.jpg", "text", 7),
-                      albumContainer("assets/images/fourth.jpg", "text", 7),
-                      albumContainer("assets/images/fifth.jpg", "text", 7),
-                      albumContainer("assets/images/sixth.jpg", "text", 7),
-                      albumContainer("assets/images/seventh.jpg", "text", 7),
-                      albumContainer("assets/images/eightth.jpg", "text", 7),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Recently Music",
-                    style: TextStyle(
-                        color: Colors.white, fontSize: 25, fontFamily: "Kanit"),
-                  ),
-                ),
-              ],
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (value) => {},
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Colors.red,
+          unselectedItemColor: Color.fromRGBO(44, 49, 69, 1),
+          backgroundColor: Color.fromRGBO(26, 31, 60, 1),
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
             ),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Colors.red,
-        unselectedItemColor: Color.fromRGBO(44, 49, 69, 1),
-        backgroundColor: Color.fromRGBO(26, 31, 60, 1),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore_outlined),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Colors.grey,
-            icon: Icon(Icons.area_chart),
-            label: 'Trend',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chat',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Profile',
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore_outlined),
+              label: 'Explore',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: Colors.grey,
+              icon: Icon(Icons.area_chart),
+              label: 'Trend',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -128,21 +157,62 @@ Widget albumContainer(imgPath, text, n) {
   return Stack(children: [
     Container(
       margin: EdgeInsets.all(5),
-      child: Image.asset(imgPath),
+      child: Image.asset(
+        imgPath,
+      ),
       height: 75,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5), color: Colors.white),
       width: 100,
     ),
+    Container(
+        alignment: Alignment.bottomLeft,
+        margin: EdgeInsets.only(top: 50, bottom: 10, left: 20),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                fontFamily: "Bold", fontSize: 12, color: Colors.white),
+          ),
+        ))
   ]);
 }
 
-Widget recentMusic(imgPath, text, n) {
+Widget recentMusic(imgPath, text1, text2, n) {
   return Row(
     children: [
-      Text(n),
-      Image.asset(imgPath),
-      Text(text)
+      Container(
+          margin: EdgeInsets.only(left: 3,right: 0),
+          child: Center(
+              child: Text(
+            n,
+            style: TextStyle(fontSize: 15, color: Colors.white70),
+          ))),
+      Expanded(
+        child: ListTile(
+          leading: Container(
+            child: Image.asset(
+              imgPath,
+              height: 42,
+              width: 42,
+            ),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+          ),
+          title: Text(
+            text1,
+            style: TextStyle(color: Colors.white, fontFamily: "Medium"),
+          ),
+          subtitle: Text(
+            text2,
+            style: TextStyle(color: Colors.grey, fontSize: 11),
+          ),
+          onTap: () => {
+            Size.fromHeight(20),
+          },
+          trailing: Icon(Icons.more_horiz,color: Colors.white,),
+          // leading: CircleAvatar(backgroundImage: AssetImage('assets/images/Home/FastWorkout.png'),),
+        ),
+      ),
     ],
   );
 }
